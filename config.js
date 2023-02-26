c1ba8e7a92eed3284574c700e4f74b07d4581412
buithien <thien7278@gmail.com>
2023-02-26 22:59:09 +0700

    version 1

---
+++ config.js
@@ -0,0 +1,25 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'bấm vào đây đi hihi',
+    title:'Cần ai đó quan tâm, để ý cái ví của mình đang hết.',
+    title:'Bước chân ra đời, sao biết đời hối hả. Sống không tiền thì vất vả vậy thôi!',
+    desc:'Bao nhiêu buồn phiền, có tiền là bay hết cho anh vay 5 lít',
+    // title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    // desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    // btnYes: 'Vẫn cứ là thích anh <33',
+    // btnNo: 'Không, Anh trai à :3',
+    btnYes: 'OK em chuyển khoản luôn! <33',
+    btnNo: 'Không, Anh trai à :3',
+    // question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại cho anh vay <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/buithien258689/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
